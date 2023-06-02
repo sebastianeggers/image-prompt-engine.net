f1707f08a1e7dda1a6ca9f772b634be6f91892bd
Sebastian Eggers <sebastian.eggers@web.de>
2023-06-03 00:22:40 +0200

    Add video game styles

--- frontend/src/data/styleData.ts
+++ frontend/src/data/styleData.ts
@@ -2,6 +2,7 @@ import { Category } from "../types";
 import { artStylesData } from "./artStylesData";
 import { artTechniquesData } from "./artTechniquesData";
 import { artistsData } from "./artistsData";
+import { videoGamesData } from "./videoGamesData";
 
 const sortCategoryStylesByTitle = (category: Category) => {
     category.styles.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
@@ -10,9 +11,11 @@ const sortCategoryStylesByTitle = (category: Category) => {
 sortCategoryStylesByTitle(artistsData);
 sortCategoryStylesByTitle(artStylesData);
 sortCategoryStylesByTitle(artTechniquesData);
+sortCategoryStylesByTitle(videoGamesData);
 
 export const styleData: Array<Category> = [
     artistsData,
     artStylesData,
     artTechniquesData,
+    videoGamesData,
 ];
